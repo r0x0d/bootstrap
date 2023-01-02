4d155cdc6357ec115682863f1b0cdf5a829903bc
Rodolfo Olivieri <rolivier@redhat.com>
2023-01-02 17:35:32 -0300

    Add neovim configs
    Signed-off-by: Rodolfo Olivieri <rolivier@redhat.com>

---
+++ files/nvim/after/plugin/coc.lua
@@ -0,0 +1,1 @@
+vim.g.coc_global_extensions = {'coc-json', 'coc-git', '@yaegassy/coc-ansible', 'coc-docker', 'coc-sh', 'coc-yaml'}
---
+++ files/nvim/lua/r0x0d/init.lua
@@ -0,0 +1,4 @@
+require("r0x0d.functions")
+require("r0x0d.packer")
+require("r0x0d.vim")
+require("r0x0d.packages")
